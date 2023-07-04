d04a78a8325ce9a95f7deddb7ca9c6a009071fe3
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-07-04 15:12:25 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Home, ExerciseDetail, Footer, Navbar } from './components'
 function App() {
   return (
     //box instead of div
-    <Box width={400}>
+    <Box width={400} sx ={{width: {x1:'1488px'}}} m ="auto">
         <Navbar/>
         <Routes>
             <Route path="/" element={<Home/>} />
