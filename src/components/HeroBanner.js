8c80d3f76d9a6de111f50dccfa4e615ff74588e8
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-07-08 21:05:39 -0500

    staring search

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -27,16 +27,23 @@ function HeroBanner() {
             Pump Iron, Feel the Pump <br/> & Get Shreded
         </Typography>
 
-        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px"
+        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" mb={5}
         >
             here are the big mass gainer movements
         </Typography>
         
-        <Typography
-         fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-                THE PUMP
+        <Button
+            variant='contained'
+            color='error'
+            href='#excersise'
+            sx={{background: '#ff2625',
+                padding: '10px'}}
+        >
+            Power Start Here
+        </Button>
+        <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+            Power
         </Typography>
-        
         <img className="hero-banner-img"  src={HeroBannerImage} alt="hero-banner" />
     </Box >
   )
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,31 @@
 
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+
 
 function SearchExercises() {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      
+      <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'30px'}}}
+          mb='50px' textAlign='center'
+      >
+        Best Pump Workout <br />
+        Feel The Pump
+      </Typography>
+      <Box position="relative" mb="72">
+        <TextField
+          height="76px"
+          value=""
+          onChange={(e)=>{}}
+          placeholder='Search for the Pump'
+        >
+
+        </TextField>
+      </Box>
+
+    </Stack>
   )
 }
 
